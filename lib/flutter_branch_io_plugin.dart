b3fcb78d0e6515effb949e187346c101460cb67f
Angga Dwi Arifandi <angga.dwi@staff.gramedia.com>
2019-02-25 10:51:24 +0700

    fix for ios method not implemented

--- lib/flutter_branch_io_plugin.dart
+++ lib/flutter_branch_io_plugin.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:io';
 
 import 'package:flutter/services.dart';
 import 'package:flutter_branch_io_plugin/flutter_branch_io_plugin.dart';
@@ -16,7 +17,7 @@ class FlutterBranchIoPlugin {
   static Stream<String> generatedLinkStream;
 
   static void setupBranchIO() {
-    _messageChannel.invokeMethod('initBranchIO');
+    if (Platform.isAndroid) _messageChannel.invokeMethod('initBranchIO');
   }
 
   static void generateLink(
@@ -29,16 +30,19 @@ class FlutterBranchIoPlugin {
       Map<String, dynamic> lpControlParams,
     }
   ) async {
-    Map<String, dynamic> _params = {};
-    String buoJson = buo.toJson();
-    _params['buoJson'] = buoJson;
-    if (lpChannel != null) _params['lp_channel'] = lpChannel;
-    if (lpFeature != null) _params['lp_feature'] = lpFeature;
-    if (lpCampaign != null) _params['lp_campaign'] = lpCampaign;
-    if (lpStage != null) _params['lp_stage'] = lpStage;
-    if (lpControlParams != null) _params['lp_control_params'] = lpControlParams;
+    if (Platform.isAndroid) {
+      Map<String, dynamic> _params = {};
+      String buoJson = buo.toJson();
+      _params['buoJson'] = buoJson;
+      if (lpChannel != null) _params['lp_channel'] = lpChannel;
+      if (lpFeature != null) _params['lp_feature'] = lpFeature;
+      if (lpCampaign != null) _params['lp_campaign'] = lpCampaign;
+      if (lpStage != null) _params['lp_stage'] = lpStage;
+      if (lpControlParams != null)
+        _params['lp_control_params'] = lpControlParams;
 
-    _messageChannel.invokeMethod("generateLink", _params);
+      _messageChannel.invokeMethod("generateLink", _params);
+    }
   }
 
   static void listOnGoogleSearch(
@@ -51,46 +55,59 @@ class FlutterBranchIoPlugin {
         Map<String, dynamic> lpControlParams,
       }
     ) async {
-    Map<String, dynamic> _params = {};
-    String buoJson = buo.toJson();
-    _params['buoJson'] = buoJson;
-    if (lpChannel != null) _params['lp_channel'] = lpChannel;
-    if (lpFeature != null) _params['lp_feature'] = lpFeature;
-    if (lpCampaign != null) _params['lp_campaign'] = lpCampaign;
-    if (lpStage != null) _params['lp_stage'] = lpStage;
-    if (lpControlParams != null) _params['lp_control_params'] = lpControlParams;
+    if (Platform.isAndroid) {
+      Map<String, dynamic> _params = {};
+      String buoJson = buo.toJson();
+      _params['buoJson'] = buoJson;
+      if (lpChannel != null) _params['lp_channel'] = lpChannel;
+      if (lpFeature != null) _params['lp_feature'] = lpFeature;
+      if (lpCampaign != null) _params['lp_campaign'] = lpCampaign;
+      if (lpStage != null) _params['lp_stage'] = lpStage;
+      if (lpControlParams != null)
+        _params['lp_control_params'] = lpControlParams;
 
-    _messageChannel.invokeMethod("listOnGoogleSearch", _params);
+      _messageChannel.invokeMethod("listOnGoogleSearch", _params);
+    }
   }
 
   static void trackContent(
       FlutterBranchUniversalObject buo,
       String eventType
   ) {
-    Map<String, dynamic> _params = {};
-    String buoJson = buo.toJson();
-    _params['buoJson'] = buoJson;
-    _params['eventType'] = eventType;
+    if (Platform.isAndroid) {
+      Map<String, dynamic> _params = {};
+      String buoJson = buo.toJson();
+      _params['buoJson'] = buoJson;
+      _params['eventType'] = eventType;
 
-    _messageChannel.invokeMethod("trackContent", _params);
+      _messageChannel.invokeMethod("trackContent", _params);
+    }
   }
 
   static void setUserIdentity( String userId ) {
-    Map<String, dynamic> _params = {};
-    _params["userId"] = userId;
-    _messageChannel.invokeMethod("setUserIdentity", _params);
+    if (Platform.isAndroid) {
+      Map<String, dynamic> _params = {};
+      _params["userId"] = userId;
+      _messageChannel.invokeMethod("setUserIdentity", _params);
+    }
   }
 
   static void clearUserIdentity( String userId ) {
-    _messageChannel.invokeMethod("clearUserIdentity");
+    if (Platform.isAndroid) _messageChannel.invokeMethod("clearUserIdentity");
   }
 
   static Future<String> getLatestParam() async {
-    return await _messageChannel.invokeMethod('getLatestParam');
+    if (Platform.isAndroid) {
+      return await _messageChannel.invokeMethod('getLatestParam');
+    }
+    return "";
   }
 
   static Future<String> getFirstParam() async {
-    return await _messageChannel.invokeMethod('getFirstParam');
+    if (Platform.isAndroid) {
+      return await _messageChannel.invokeMethod('getFirstParam');
+    }
+    return "";
   }
 
   static Stream<String> listenToDeepLinkStream() {
@@ -99,6 +116,7 @@ class FlutterBranchIoPlugin {
   }
 
   static Stream<String> listenToGeneratedLinkStream() {
+    if (Platform.isIOS) return Stream.empty();
     if ( generatedLinkStream == null ) generatedLinkStream = _generatedLinkChannel.receiveBroadcastStream().cast<String>();
     return generatedLinkStream;
   }
